7c933942d18cc93a8959d77a2b245257f7288c44
sjburwell <scott.j.burwell@gmail.com>
2022-04-12 21:13:19 -0500

    added Babel to transpile on the fly, added ReactDOM render to render

--- exercise-files/voting_app/public/js/app-a1.js
+++ exercise-files/voting_app/public/js/app-a1.js
@@ -1,3 +1,6 @@
+// the React library was already imported as a script in the index.html file
+
+// this defines the Component
 class ProductList extends React.Component {
   render() {
     return (
@@ -7,3 +10,11 @@ class ProductList extends React.Component {
     );
   }
 }
+
+// This renders the component, without it, the JSX code won't work
+// The first argument specifies the target element in the index.html file (i.e., WHAT)
+// The second argument specifies the component to render (i.e., WHERE), i.e., in the "content" div of the index.html file
+ReactDOM.render(
+  <ProductList />,
+  document.getElementById('content')
+);
