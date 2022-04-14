10d008dcc8692b84244fdf54a243cbf41a2219ac
sjburwell <scott.j.burwell@gmail.com>
2022-04-14 17:44:18 -0500

    moved to app.js, added counter for upvoting

--- exercise-files/voting_app/public/js/app.js
+++ exercise-files/voting_app/public/js/app.js
@@ -1,59 +1,67 @@
 
 class ProductList extends React.Component {
-
   handleProductUpVote(productId) {
     console.log(productId + ' was upvoted.');
   }
 
   render() {
-    const products = Seed.products.sort((a, b) => (
-      b.votes - a.votes
-    ));
+    // addition of sort will be mutable, which can be dangerous but explained later.
+    // sort 
+    const products = Seed.products.sort((a, b) => b.votes - a.votes);
     const productComponents = products.map((product) => (
-      <Product
-        key={'product-' + product.id}
-        id={product.id}
-        title={product.title}
-        description={product.description}
-        url={product.url}
-        votes={product.votes}
-        submitterAvatarUrl={product.submitterAvatarUrl}
-        productImageUrl={product.productImageUrl}
-        onVote={this.handleProductUpVote}
-      />
-    ));
+      <Product 
+          id={product.id}
+          title={product.title}
+          description={product.description}
+          url={product.url}
+          votes={product.votes}
+          submitterAvatarUrl={product.submitterAvatarUrl}
+          productImageUrl={product.productImageUrl}
+          onVote={this.handleProductUpVote}
+        />
+    )
+    );
     return (
-      <div className='ui unstackable items'>
+      <div className="ui unstackable items">
         {productComponents}
       </div>
     );
   }
 }
 
+
 class Product extends React.Component {
+  
+  // defines the added function 
+  constructor(props) {
+    super(props);
+    this.handleUpVote = this.handleUpVote.bind(this);
+  }
+
+  // defines the added function
   handleUpVote() {
     this.props.onVote(this.props.id);
   }
+
   render() {
     return (
+        // must use className instead of class because class is a reserved word in javascript
       <div className='item'>
         <div className='image'>
           <img src={this.props.productImageUrl} />
         </div>
         <div className='middle aligned content'>
           <div className='header'>
-            <a onClick={this.handleUpVote}>
-              <i className='large caret up icon' />
-            </a>
-            {this.props.votes}
+              <a onClick={this.handleUpVote}>
+                  <i className='large caret up icon' /> 
+              </a>
+              {this.props.votes}
           </div>
           <div className='description'>
-            <a href={this.props.url}>
-              {this.props.title}
-            </a>
-            <p>
-              {this.props.description}
-            </p>
+              <a href={this.props.url}>
+                  {this.props.title}
+              </a>
+              <p>{this.props.description}</p>
           </div>
           <div className='extra'>
             <span>Submitted by:</span>
